67825426d85ef08c0d4435a7e796cd8cf2706b70
Cristi Scheye <cristi.scheye@gmail.com>
2015-05-05 10:53:02 -0700

    linter fixes

--- test/test.jsx
+++ test/test.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 import {addons} from 'react/addons'
 import Button from '../index.jsx'
 const {TestUtils} = addons
-const {Simulate, renderIntoDocument, isElement, createRenderer} = TestUtils
-// const getReactNode = (dom, node) => TestUtils.findRenderedDOMComponentWithTag(dom, node)
-// const getDOMNode = (dom, node) => getReactNode(dom, node).getDOMNode()
-// const getDOMNodes = (dom, type) => TestUtils.scryRenderedDOMComponentsWithTag(dom, type)
-// const getDOMNodeText = (dom, node) => getDOMNode(dom, node).textContent
+const {isElement} = TestUtils
 
 test('Button: constructor', (t) => {
   const button = React.createElement(Button)
