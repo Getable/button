6322bec3d95749298cbdc4ba06c14dcd2eb5b7da
Cristi Scheye <cristi.scheye@gmail.com>
2015-05-19 10:53:29 -0700

    change entry files

--- example/data.js
+++ example/data.js
@@ -1,5 +1,5 @@
 export default {
-  classes: ['button-cta']
+  classes: ['btn--grey', 'hollow', 'icon-more', 'btn--fullCenter']
   , text: 'Click me'
   , onClick: function(){
       window.alert('You\'ve won $500! Click here to claim your prize!')
